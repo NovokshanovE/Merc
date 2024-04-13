916a8e9cab8aaa1500effe8f65cfc8044009d874
Кильдишев Петр <epic.rabbit@mail.ru>
2024-04-13 13:12:30 +0300

    Now server returns json

--- API/ML/api.py
+++ API/ML/api.py
@@ -1,2 +1,11 @@
+import json
+
+
 def sertificate_image(image):
     print(f"Определяем тип документа:\n{image}")
+    return json.dumps({
+        'doctype': 'test',
+        'page': 0,
+        'series': 4000,
+        'number': 2000
+    })
